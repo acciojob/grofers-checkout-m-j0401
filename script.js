b542ebc78c69390c2d36e9d96d705180eebe83ff
m-j0401 <mehul.jain2206@gmail.com>
2026-01-18 15:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let tab=document.querySelector("table")
+let prc=document.querySelectorAll(".price")
 
 const getSum = () => {
-//Add your code here
+//Add your code her
+	let sum=0;
+	for(let p of prc)
+		{
+			sum+=+p.innerText;
+		}
+	let new_row=document.createElement("tr");
+	let td1=document.createElement("td");
+	let td2=document.createElement("td");
+	td1.innerText="Total Price";
+	td2.innerText=sum;
+	new_row.append(td1,td2);
+	tab.append(new_row);
+	
+	
   
 };
 
